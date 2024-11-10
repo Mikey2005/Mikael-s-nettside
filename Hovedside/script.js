b398f70a57eef1e751ee34354b03e0fe703e7a7a
mikael <miseb2005@gmail.com>
2024-11-10 13:35:12 +0100

    Added a new profile page, and added some Javascript to make a drop down menu when clicking profile picture

---
+++ Hovedside/script.js
@@ -0,0 +1,23 @@
+const profilePictrue = document.querySelector(".profile-picture img");
+const dropdownMenu = document.getElementById("dropdownmenu");
+const viewProfileButton = document.getElementById("view-profile");
+const logoutButton = document.getElementById("logout");
+
+profilePictrue.addEventListener("click", () => {
+    dropdownMenu.style.display = dropdownMenu.style.display === "block" ? "none" : "block";
+
+});
+
+document.addEventListener("click", (event) => {
+    if (!profilePictrue.contains(event.target) && !dropdownMenu.contains(event.target)) {
+        dropdownMenu.style.display = "none";
+    }
+});
+
+viewProfileButton.addEventListener("click", () => {
+    window.location.href = "/profil/profilside.html";
+});
+
+logoutButton.addEventListener("click", () => {
+    window.location.href = "/loggut.html"
+})
